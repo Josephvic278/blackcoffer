a3672fc50f04740d49be5ffb1ab621ba6bc1be9b
jvictory225@gmail.com <joseph>
2024-07-05 11:38:40 +0100

    new update

--- blackcoffer/app/views.py
+++ blackcoffer/app/views.py
@@ -5,38 +5,5 @@ import time
 
 # Create your views here.
 def dashboard(request):
-    jd = ""
-    with open("/storage/emulated/0/jsondata.json", "r") as d:
-        jd = json.load(d)
-    for get in jd:
-        print(get["title"])
-        end_year = get["end_year"]
-        if end_year == "":
-            end_year = None
-        intensity = get["intensity"]
-        if intensity == "":
-            intensity = None
-        sector = get["sector"] 
-        topic = get["topic"]
-        insight = get["insight"]
-        url = get["url"]
-        region = get["region"]
-        start_year = get["start_year"]
-        if start_year == "":
-            start_year = None
-        impact = get["impact"]
-        added = get["added"]
-        published = get["published"]
-        country = get["country"]
-        relevance = get["relevance"]
-        pestle = get["pestle"]
-        title = get["title"]
-        likelihood = get["likelihood"]
-            
-        new_info = Info(end_year = end_year, intensity = intensity,sector = sector, topic = topic, insight = insight, url = url, region = region, start_year = start_year, impact = impact, added = added, published = published, country = country, relevance = relevance, pestle = pestle, title = title,  likelihood = likelihood)
-        new_info.save()
-        
-        print("Saved!!!", print(new_info))
-        #time.sleep(1)
-    print(Info.objects.all())
-    return render(request, "dashboard.html")
+    model_data =  Info.objects.all()
+    return render(request, "dashboard.html", {'data':model_data})
